be846617de3ed8fcca75b21ba8ab20efeca0df68
Marcos Kilmer <marcoskilmer1@gmail.com>
2023-05-19 16:41:15 -0300

    SearchUserDisplayProtocol initial created

--- GitHubApp/App/SearchUser/Presenter/UserSearchPresenter.swift
+++ GitHubApp/App/SearchUser/Presenter/UserSearchPresenter.swift
@@ -13,11 +13,17 @@ protocol UserSearchPresenterProtocol {
 }
 
 class UserSearchPresenter: UserSearchPresenterProtocol {
+    weak var view: SearchUserDisplayProtocol?
+    
+    init(view: SearchUserDisplayProtocol) {
+        self.view = view
+    }
+    
     func presentUserList(_ users: [User]) {
-        
+        view?.displayUserList(with: users)
     }
     
     func presentErrorUserList(_ error: Error) {
-        
+        view?.displayErrorUserList(with: error)
     }
 }
--- GitHubApp/App/SearchUser/ViewController/SearchUserViewController.swift
+++ GitHubApp/App/SearchUser/ViewController/SearchUserViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
+protocol SearchUserDisplayProtocol: AnyObject {
+    func displayUserList(with users: [User])
+    func displayErrorUserList(with error: Error)
+}
+
 class SearchUserViewController: UIViewController {
     private let customView = SearchUserView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view = customView
+    }
+}
+
+// MARK: - SearchUserDisplayProtocol
+extension SearchUserViewController: SearchUserDisplayProtocol {
+    func displayUserList(with users: [User]) {
+        
+    }
+    
+    func displayErrorUserList(with error: Error) {
+        
     }
 }
