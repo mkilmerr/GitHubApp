d9aea59235b062d7f50ee321131cbcbc443b68f4
Marcos Kilmer <marcoskilmer1@gmail.com>
2023-05-19 16:29:49 -0300

    UserSearchPresenter initial structure created

---
+++ GitHubApp/App/SearchUser/Presenter/UserSearchPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  UserSearchPresenter.swift
+//  GitHubApp
+//
+//  Created by Marcos Kilmer  on 19/05/23.
+//
+
+import Foundation
+
+protocol UserSearchPresenterProtocol {
+    func presentUserList(_ users: [User])
+}
+
+class UserSearchPresenter: UserSearchPresenterProtocol {
+    func presentUserList(_ users: [User]) {
+        
+    }
+}
