d9c30e8218d010e30b1c6d4c497febc01bce57b1
Marcos Kilmer <marcoskilmer1@gmail.com>
2023-05-19 15:28:08 -0300

    SearchUserService initial structure created

---
+++ GitHubApp/App/SearchUser/Service/SearchUserService.swift
@@ -0,0 +1,29 @@
+//
+//  SearchUserService.swift
+//  GitHubApp
+//
+//  Created by Marcos Kilmer  on 19/05/23.
+//
+
+import Foundation
+
+protocol SearchUserServiceProtocol: AnyObject {
+    var baseURL: String { get set }
+    func loadUsersDefaultList()
+}
+
+class SearchUserService: SearchUserServiceProtocol {
+    var baseURL: String = "https://api.github.com"
+    private lazy var httpGetManager = HTTPGetManager(url: baseURL)
+    
+    func loadUsersDefaultList() {
+        httpGetManager.request { result in
+            switch result {
+            case .success(let data) :
+                break
+            case .failure(let error):
+                break
+            }
+        }
+    }
+}
